7bd7ab04545762cdf198bd78403bb10b183c84a4
janak-ojha <janakrajojha66@gmail.com>
2024-01-25 11:27:40 +0530

    hello

--- Array/BAsis_array.java
+++ Array/BAsis_array.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class BAsis_array {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.println("enter the size of array");
+        System.out.println("enter the size  array");
         int size=sc.nextInt();
         int num[]=new int[size];
         System.out.println("enter tje gi");
